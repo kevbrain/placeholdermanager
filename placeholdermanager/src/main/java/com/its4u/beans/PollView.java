05ffdca2a73d57e4049119991f8f8d65bd475b81
kevyn.schrondweiler <kevyn.schrondweiler@ITS4U173.mshome.net>
2022-01-17 12:38:37 +0100

    format

--- placeholdermanager/src/main/java/com/its4u/beans/PollView.java
+++ placeholdermanager/src/main/java/com/its4u/beans/PollView.java
@@ -35,7 +35,7 @@ public class PollView implements Serializable {
 
     public void log(String log) {    	
         
-    	logPile.push(log+"[<p style=\"color:green\">OK</p>]");
+    	logPile.push(log+"[<p style=\"color:green\">OK</p>]<br/>");
     			
     }
   
