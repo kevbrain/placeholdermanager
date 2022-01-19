5698521158ccf0d0fa74fd7b07a1e487a99952b4
kevyn.schrondweiler <kevyn.schrondweiler@ITS4U173.mshome.net>
2022-01-19 09:39:01 +0100

    add unirest dependency and function get token

--- placeholdermanager/src/main/java/com/its4u/beans/ArgoInitializerBean.java
+++ placeholdermanager/src/main/java/com/its4u/beans/ArgoInitializerBean.java
@@ -1,8 +1,19 @@
 package com.its4u.beans;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import org.primefaces.model.TreeNode;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 
 import lombok.Data;
 
@@ -20,7 +31,24 @@ public class ArgoInitializerBean {
 	private String argoPassword;
 	
 	public void testConnection() {
+		System.out.println("argo server : "+argoServer);
 		System.out.println("argo user : "+argoUser);
 		System.out.println("argo password : "+argoPassword);
+	}
+	
+	public void synchronise(String project) throws IOException {
+		
+		Unirest.setTimeouts(0, 0);
+		try {
+			HttpResponse<String> response = Unirest.post(argoServer+"/api/v1/session")
+			  .header("Content-Type", "text/plain")			  
+			  .body("{\r\n  \"password\": \""+argoPassword+"\",\r\n  \"username\": \""+argoUser+"\"\r\n}")
+			  .asString();
+			System.out.println(response.getBody());
+		} catch (UnirestException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 }
