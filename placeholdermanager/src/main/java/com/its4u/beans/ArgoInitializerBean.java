4cce6d1645989348ee04b2ccb516e4503a500e50
kevyn.schrondweiler <kevyn.schrondweiler@ITS4U173.mshome.net>
2022-01-20 11:07:26 +0100

    DSFSDFD

--- placeholdermanager/src/main/java/com/its4u/beans/ArgoInitializerBean.java
+++ placeholdermanager/src/main/java/com/its4u/beans/ArgoInitializerBean.java
@@ -6,6 +6,16 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.security.SecureRandom;
+import java.security.Security;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
 
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -67,6 +77,12 @@ public class ArgoInitializerBean {
 	    }
 		
 		System.out.println("UNIREST METHOD");
+		try {
+			doTrustToCertificates();
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		Unirest.setTimeouts(0, 0);
 		try {
 			HttpResponse<String> response = Unirest.post("https://openshift-gitops-server-openshift-gitops.apps.ocp-lab.its4u.eu/api/v1/applications/test-toto/sync")
@@ -112,4 +128,39 @@ public class ArgoInitializerBean {
 		      
 		return token;
 	}
+	
+	 // trusting all certificate 
+	 public void doTrustToCertificates() throws Exception {
+	        Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
+	        TrustManager[] trustAllCerts = new TrustManager[]{
+	                new TrustManager() {
+	                    public X509Certificate[] getAcceptedIssuers() {
+	                        return null;
+	                    }
+
+	                    public void checkServerTrusted(X509Certificate[] certs, String authType) throws CertificateException {
+	                        return;
+	                    }
+
+	                    public void checkClientTrusted(X509Certificate[] certs, String authType) throws CertificateException {
+	                        return;
+	                    }
+	                }
+	        };
+
+	        SSLContext sc = SSLContext.getInstance("SSL");
+	        sc.init(null, trustAllCerts, new SecureRandom());
+	        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+	        HostnameVerifier hv = new HostnameVerifier() {
+	           
+				@Override
+				public boolean verify(String arg0, SSLSession arg1) {
+					if (!arg0.equalsIgnoreCase(arg1.getPeerHost())) {
+	                    System.out.println("Warning: URL host '" + arg0 + "' is different to SSLSession host '" + arg1.getPeerHost() + "'.");
+	                }
+	                return true;
+				}
+	        };
+	        HttpsURLConnection.setDefaultHostnameVerifier(hv);
+	    }
 }
