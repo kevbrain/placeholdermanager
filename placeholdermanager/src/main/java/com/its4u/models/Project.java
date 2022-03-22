e043a2abf18988d20486f747a695ba10d834f14d
'ksc' <'ksc@example.com';>
2022-03-22 13:13:04 +0100

    dsfsfsd

--- placeholdermanager/src/main/java/com/its4u/models/Project.java
+++ placeholdermanager/src/main/java/com/its4u/models/Project.java
@@ -18,7 +18,7 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 
-@Data
+
 @Entity
 public class Project implements Serializable{
 	
@@ -83,6 +83,114 @@ public class Project implements Serializable{
 	
 	public String toString() {
 		return project_Id ;
+	}
+
+	public String getProject_Id() {
+		return project_Id;
+	}
+
+	public void setProject_Id(String project_Id) {
+		this.project_Id = project_Id;
+	}
+
+	public String getGitUrl() {
+		return gitUrl!=null?gitUrl:"";
+	}
+
+	public void setGitUrl(String gitUrl) {
+		this.gitUrl = gitUrl;
+	}
+
+	public String getOwner() {
+		return owner;
+	}
+
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+
+	public String getTeam() {
+		return team!=null?team:"";
+	}
+
+	public void setTeam(String team) {
+		this.team = team;
+	}
+
+	public String getValueChain() {
+		return valueChain!=null?valueChain:"";
+	}
+
+	public void setValueChain(String valueChain) {
+		this.valueChain = valueChain;
+	}
+
+	public String getDescription() {
+		return description!=null?description:"";
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<Environments> getEnvironments() {
+		return environments;
+	}
+
+	public void setEnvironments(List<Environments> environments) {
+		this.environments = environments;
+	}
+
+	public List<Versions> getVersions() {
+		return versions;
+	}
+
+	public void setVersions(List<Versions> versions) {
+		this.versions = versions;
+	}
+
+	public HashMap<String, String> getMapenvs() {
+		return mapenvs;
+	}
+
+	public void setMapenvs(HashMap<String, String> mapenvs) {
+		this.mapenvs = mapenvs;
+	}
+
+	public HashMap<String, HashMap<String, PlaceHolderSpec>> getMapPlaceHoldersByEnv() {
+		return mapPlaceHoldersByEnv;
+	}
+
+	public void setMapPlaceHoldersByEnv(HashMap<String, HashMap<String, PlaceHolderSpec>> mapPlaceHoldersByEnv) {
+		this.mapPlaceHoldersByEnv = mapPlaceHoldersByEnv;
+	}
+
+	public HashMap<String, ArgoAppStatus> getMapappstatusByEnv() {
+		return mapappstatusByEnv;
+	}
+
+	public void setMapappstatusByEnv(HashMap<String, ArgoAppStatus> mapappstatusByEnv) {
+		this.mapappstatusByEnv = mapappstatusByEnv;
+	}
+
+	public Map<String, Boolean> getEnvSelectedMap() {
+		return envSelectedMap;
+	}
+
+	public void setEnvSelectedMap(Map<String, Boolean> envSelectedMap) {
+		this.envSelectedMap = envSelectedMap;
+	}
+
+	public boolean isSynchrone() {
+		return synchrone;
+	}
+
+	public void setSynchrone(boolean synchrone) {
+		this.synchrone = synchrone;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
 	}
 	
 	
